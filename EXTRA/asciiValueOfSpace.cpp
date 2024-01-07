66d685e28466521677099dbc53b1c9a462260dee
Aman Kumar Jha <112791419+jhAman-9@users.noreply.github.com>
2024-01-07 19:07:28 +0530

    Delete EXTRA directory

--- EXTRA/asciiValueOfSpace.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    // cout<<int(' ');
-    cout<<(2%10);
-}
--- EXTRA/basic.cpp
+++
@@ -1,5 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-   cout<<int(1%2);
-}
