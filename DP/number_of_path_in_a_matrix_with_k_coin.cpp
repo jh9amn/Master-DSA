24796405e6fbf23a01f1f11153e294c0476e9d4c
Aman Kumar Jha <amankumar9631400@gmail.com>
2024-02-10 13:04:29 +0530

    java code try

--- DP/number_of_path_in_a_matrix_with_k_coin.cpp
+++ DP/number_of_path_in_a_matrix_with_k_coin.cpp
@@ -60,3 +60,65 @@ int main()
     }
 }
 // } Driver Code Ends
+
+
+
+// JAVA CODE with Memoization
+
+
+
+
+// import java.io.*;
+// import java.util.*;
+
+// class GFG
+// {
+//     public static void main(String args[])throws IOException
+//     {
+        
+//         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+//         int t = Integer.parseInt(read.readLine());
+//         while(t-- > 0)
+//         {
+            
+//             int  k = Integer.parseInt(read.readLine());
+//             int  n = Integer.parseInt(read.readLine());
+//             int arr[][] = new int[n][n];
+//             String input_line1[] = read.readLine().trim().split("\\s+");
+//             int c = 0;
+//             for(int i=0;i<n;i++){
+//                 for(int j = 0;j<n;j++){
+//                     arr[i][j] = Integer.parseInt(input_line1[c]);
+//                     c++;
+//                 }
+//             }
+//             Solution obj = new Solution();
+//             System.out.println(obj.numberOfPath(n, k, arr));
+//         }
+//     }
+// }
+
+
+
+//User function Template for Java
+
+// class Solution {
+//     int ans;
+//     long numberOfPath(int n, int k, int [][]arr) {
+//        ans = 0; 
+//        dfs(0,0,n,k,arr);
+//        return ans;
+//     }
+    
+//     void dfs(int r, int c, int n, int k, int [][]arr){
+//         if( k < 0) return;
+//         if( r < n && c < n && r >= 0 && c >= 0) {
+//             if(r == n-1 &&  c == n-1) {
+//                 if (k == arr[r][c])
+//                     ans++;
+//             }
+//             dfs(r+1, c, n , k - arr[r][c], arr);
+//             dfs(r, c+1, n, k - arr[r][c], arr);
+//         }
+//     }
+// }
