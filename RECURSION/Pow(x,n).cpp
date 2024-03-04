dbe633652d4d9a442dafd450f22133d36151c6e7
Aman Kumar Jha <amankumar9631400@gmail.com>
2024-03-04 20:27:35 +0530

    power(x,n) leetcode 50

---
+++ RECURSION/Pow(x,n).cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    double solve(double x, long n){
+        if(n == 0) return 1;
+        if(n < 0) return solve(1/x, -n);
+        if(n%2==0) return solve(x*x, n/2);
+        else return x * solve(x * x, (n-1)/2);
+    }
+    double myPow(double x, int n) {
+       return solve(x,n);
+        
+    }
+};
+
+int main(){
+
+}
