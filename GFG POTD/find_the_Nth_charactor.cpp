b28bb0e1a0a90fc99e9a38a77ee9718293f7c226
Aman Kumar Jha <amankumar9631400@gmail.com>
2024-03-09 12:22:04 +0530

    brute force apporoach and optimized apporach to find nth char from 0 ans 1

--- GFG POTD/find_the_Nth_charactor.cpp
+++ GFG POTD/find_the_Nth_charactor.cpp
@@ -10,37 +10,40 @@ class Solution{
   public:
     char nthCharacter(string s, int r, int n) {
         // Brute Force 
-        // T.C = O(r * 2 ^ r)
-        // while(r--){
-        //     string ans = "";
-        //     for(int i=0; i<s.size(); i++){
-        //         if(s[i] == '1'){
-        //             ans += "10";
-        //         }
-        //         else{
-        //             ans += "01";
-        //         }
-        //     }
-        //     s = ans;
-        // }
-        // return s[n];
+        // T.C = O(r * s.size())
+        while(r--){
+            string ans = "";
+            for(int i=0; i<s.size(); i++){
+                if(s[i] == '1'){
+                    ans += "10";
+                }
+                else{
+                    ans += "01";
+                }
+                // whenever my ans.size  > s.size then break because we need that char which is under the s.size
+                // no need to generate string more than s.size because ( n  < s.size)
+                if(ans.size() > s.size()) break;
+            }
+            s = ans;
+        }
+        return s[n];
         
         
         // Optimazation Approach
         // T.C = O(r)       S.C.= auxilary space 
-        if(r == 0) return s[n];
-        char c = nthCharacter(s , r-1, n /2);
-        if( n % 2 == 1) {
-            // right side char 
-            if( c == '0') return '1';  // next time it produce just opposite of its previous round index
-            else return '0';
-        }
+        // if(r == 0) return s[n];
+        // char c = nthCharacter(s , r-1, n /2);
+        // if( n % 2 == 1) {
+        //     // right side char 
+        //     if( c == '0') return '1';  // next time it produce just opposite of its previous round index
+        //     else return '0';
+        // }
         
-        else{
-            // left side char
-            if( c == '0') return '0';  // next time it also produce same value at left/even index
-            else return '1';
-        }
+        // else{
+        //     // left side char
+        //     if( c == '0') return '0';  // next time it also produce same value at left/even index
+        //     else return '1';
+        // }
     }
 };
 
