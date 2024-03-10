0d2aa3b290f4334a3d27e53df4109e4e87b1d66b
Aman Kumar Jha <amankumar9631400@gmail.com>
2024-03-10 08:27:53 +0530

    Remove all the dupicate from the given string

---
+++ GFG POTD/Remove_all_the_duplicate_from_the_given_string.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	string removeDuplicates(string str) {
+	    map<char,int> mp;
+	    
+	    for(int i=0 ; i<str.size(); i++){
+	        mp[str[i]]++;
+	    }
+	    
+	    string ans = "";
+	    for(int i=0; i<str.size(); i++){
+	        if(mp[str[i]] >= 1){
+	            ans += str[i];
+	            mp[str[i]] = 0;
+	        }
+	    }
+	    return ans;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        auto ans = ob.removeDuplicates(str);
+
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
