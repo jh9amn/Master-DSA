c994f616a0e8dc2b54fca81d32bac3a8a3e4f4a8
Aman Kumar Jha <amankumar9631400@gmail.com>
2024-03-11 20:21:11 +0530

    Custon Sort String leetcode 791

--- LEETCODE/MEDIUM/Custom_sort_string_791.cpp
+++ LEETCODE/MEDIUM/Custom_sort_string_791.cpp
@@ -1,35 +1,42 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     string customSortString(string order, string s) {
         string ans = "";
-        vector<int> v(26,0);
-        for(int i=0; i<s.size(); i++){
-            v[s[i]- 'a']++;
-        }
+//         vector<int> v(26,0);
+//         for(int i=0; i<s.size(); i++){
+//             v[s[i]- 'a']++;
+//         }
         
+//         for(int i=0; i<order.size(); i++){
+//             int idx = order[i] - 'a';
+//             if(v[idx] > 0){
+//                 while(v[idx]--){
+//                     ans += order[i];
+//                 }
+//                 v[idx] = -1;
+//             }
+//         }
+        
+//         for(int i=0; i<s.size(); i++){
+//             if(v[s[i] - 'a'] != -1) ans += s[i];
+//         }
+//      return ans;
+        
+        
+        // second Method Using Custom Sorting
+        // T.C = O(NlogN)   S.C = O(26) ~ O(1)
+        vector<int> cusOrder(26,-1);
+        int step = 1;
+        // Here storing which char comes First
         for(int i=0; i<order.size(); i++){
-            int idx = order[i] - 'a';
-            if(v[idx] > 0){
-                while(v[idx]--){
-                    ans += order[i];
-                }
-                v[idx] = -1;
-            }
+            cusOrder[order[i] - 'a'] = step;
+            step++;
         }
         
-        for(int i=0; i<s.size(); i++){
-            if(v[s[i] - 'a'] != -1) ans += s[i];
-        }
-        
-        return ans;
+        // sort whose come after then remain same
+        sort(s.begin(), s.end(), [&cusOrder] (char a, char b){
+            return cusOrder[a - 'a'] < cusOrder[b - 'a'] ? true : false;
+        });
+        return s;
     }
 };
-
-
-int main(){
-
-
-}
