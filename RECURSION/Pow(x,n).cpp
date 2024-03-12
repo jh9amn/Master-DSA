6a6a5ac74cabfd3661e7c2ff3611c462d6adba04
Aman Kumar Jha <amankumar9631400@gmail.com>
2024-03-12 10:39:28 +0530

    Pow(x,n) in logN

--- RECURSION/Pow(x,n).cpp
+++ RECURSION/Pow(x,n).cpp
@@ -5,11 +5,19 @@ using namespace std;
 // T.C = O(logN)
 class Solution {
 public:
+    // T.C = O(logN)
     double solve(double x, long n){
-        if(n == 0) return 1;
-        if(n < 0) return solve(1/x, -n);
-        if(n%2==0) return solve(x*x, n/2);
-        else return x * solve(x * x, (n-1)/2);
+        if( n == 0) return 1;
+        if( n == -1) return 1/x;
+        
+        double temp = solve(x, n/2);
+        
+        // even
+        if(n%2 == 0) return temp * temp;
+        // odd
+        if(n > 0 ) return temp * temp * x;
+        // negative
+        return temp * temp * (1/x);
     }
     double myPow(double x, int n) {
        return solve(x,n);
