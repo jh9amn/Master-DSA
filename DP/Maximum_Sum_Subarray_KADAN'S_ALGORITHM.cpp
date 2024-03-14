5542ae817203c26dfa8a8d8236073dd70140136c
Aman Kumar Jha <amankumar9631400@gmail.com>
2024-03-14 12:54:37 +0530

    KADAN'S ALGO maxi sum subarray

---
+++ DP/Maximum_Sum_Subarray_KADAN'S_ALGORITHM.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+//      Method 1  T.C = O(n*n)    TLE
+//         int n = nums.size();
+//         int maxi = INT_MIN;
+//         for(int i=0; i<n; i++){
+//             int sum = 0;
+//             for(int j=i; j<n; j++){
+//                 sum += nums[j];
+//                 maxi = max(sum, maxi);
+
+//             }
+//         }
+//         return maxi;
+        
+
+//          MEthod 2  (KADAN'S ALGORITHM)
+//         T.C = O(n)
+        int maxi = INT_MIN;
+        int sum = 0;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            sum += nums[i];
+            
+            maxi = max(maxi, sum);
+            
+            if(sum < 0) sum = 0;
+        }
+        return maxi;
+    }
+};
+
+
+int main(){
+
+
+}
