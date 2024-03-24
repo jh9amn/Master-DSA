eb411bacf4bd8ee8c30d1e4a59132a0f31f2613c
Aman Kumar Jha <amankumar9631400@gmail.com>
2024-03-24 13:07:00 +0530

    Duplicate Elememt In array

--- LEETCODE/MEDIUM/Fiind_Duplicate_Element.cpp
+++ LEETCODE/MEDIUM/Fiind_Duplicate_Element.cpp
@@ -1,36 +1,55 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    int BS( int start, int end, vector<int>& v, int x){
-        while(start <= end){
-            int mid = start + (end - start)/2;
+    int findDuplicate(vector<int>& nums) { 
+//      Using Binary Search Apporach     T.C = O ( n * log(n))
+//         int start = 1;
+//         int end = nums.size() -1;
+        
+//         while(start < end){
+//             int mid = start + (end - start)/2;
+//             int count = 0;
             
-            if(v[mid] == x) return x;
-            
-            else if(v[mid] > x){
-                end = mid -1;
-            }
-            else {
-                start = mid + 1;
-            }
+//             for(auto it : nums){
+//                 if(mid >= it){
+//                     count++;
+//                 }
+//             }
+//             if(count > mid){
+//                 end = mid;
+//             }
+//             else {
+//                 start = mid + 1;
+//             }
+//         }
+//         return start;
+        
+
+        
+//      Two Pointer Approach  T.C = O (N)  S.C = O (1)
+//         int slow = nums[0];
+//         int fast = nums[0];
+//         do {
+//             slow = nums[slow];
+//             fast = nums[nums[fast]];
+//         }
+//         while(slow != fast);
+        
+//         fast = nums[0];
+//         while( slow != fast){
+//             slow = nums[slow];
+//             fast = nums[nums[fast]];
+//         }
+//         return slow;
+        
+
+        
+// One pointer Approach but modification in array    T.C = O (N)  S.C = O (1)
+        int i=0;
+        while (nums[i] > 0){
+            int tmp = nums[i];
+            nums[i] = 0;
+            i = tmp;
         }
-         return -1;
-    }
-    
-    
-    int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] == BS(i+1,nums.size() - 1, nums, nums[i])) return nums[i];
-        }
-        return -1;
+        return i;
     }
 };
-
-
-int main(){
-
-
-}
