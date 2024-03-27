b3a03709e5e48e414b63d76486ae973381ed96f9
Aman Kumar Jha <amankumar9631400@gmail.com>
2024-03-27 23:47:23 +0530

    Subarray product lesst than k  leetcode 713

---
+++ SLIDING WINDOW/Subarray_Porduct_less_than_K.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        // product < k
+        if( k <= 1) return 0;
+        
+        int i = 0;
+        int j = 0;
+        
+        int n = nums.size();
+        int count = 0;
+        int product = 1;
+        
+        while(j < n){
+            product *= nums[j];
+            // if( product >= k) i++;
+            while(product >= k){
+                product /= nums[i];
+                i++;
+            }
+            
+            count += (j - i + 1);
+            j++;
+        }
+        return count;
+    }
+};
+
+int main(){
+
+
+}
