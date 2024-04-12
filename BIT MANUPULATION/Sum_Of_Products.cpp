f11df31b6175f185c23d1302f181bc7f286b05e3
Aman Kumar Jha <amankumar9631400@gmail.com>
2024-04-12 21:24:27 +0530

    Sum of products gfg potd

---
+++ BIT MANUPULATION/Sum_Of_Products.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+  // T.C = O(n)      S.C = O(1)
+    long long pairAndSum(int n, long long arr[]) {
+        long long ans = 0;
+        
+        for(int i=0; i<32; i++){
+            long k = 0;
+            // find pair of all num of ith bit
+            for(int j=0; j<n; j++){
+                if((arr[j] & (1<<i)) != 0) k++;
+            }
+            //     bit val *  No of possible pairs
+            ans += (1<<i) * (k * (k-1))/2;          
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        long long Arr[N];
+        for (int i = 0; i < N; i++) {
+            cin >> Arr[i];
+        }
+        Solution ob;
+        cout << ob.pairAndSum(N, Arr) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
