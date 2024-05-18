1634e68b8c5f3b11cc8ff49e4589c692a8817205
Aman Kumar Jha <amankumar9631400@gmail.com>
2024-05-18 08:09:51 +0530

    Find the Highest Number gfg potd

---
+++ GFG POTD/Find_the_Highest_Number.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    int findPeakElement(vector<int>& a) 
+    {
+        int n = a.size();
+        
+        int maxi = 0;
+        for(int i=n-1; i>=0; i--){
+            if(a[i] > a[i-1]) {
+                maxi = max(a[i], maxi);
+                break;
+            }
+        }
+        return maxi;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	vector<int>a(n);
+    	for(int i = 0; i < n; i++)
+    		cin>>a[i];
+    	Solution ob;
+    	int ans = ob.findPeakElement(a);
+    	cout << ans << "\n";
+    }
+	return 0;
+}
+
+
+// } Driver Code Ends
