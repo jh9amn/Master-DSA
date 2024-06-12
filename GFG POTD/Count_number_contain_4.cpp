df40fba584ff8e86f6b75d89b9994be84f994310
Aman Kumar Jha <amankumar9631400@gmail.com>
2024-06-12 21:34:00 +0530

    count the number containing 4 gfg potd

---
+++ GFG POTD/Count_number_contain_4.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int countNumberswith4(int n) {
+        int count = 0;
+        for(int i=4; i<=n; i++){
+            int j = i;
+            while(j){
+                if(j%10 == 4) {     // when lastly remainder 4 and this is last 4 then break
+                    count++;
+                    break;
+                }
+                
+                j /= 10;
+            }
+        }
+        return count;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        Solution obj;
+        int res = obj.countNumberswith4(n);
+
+        cout << res << endl;
+    }
+}
+
+// } Driver Code Ends
