c60fde3ac96857b714883ab4e57cc638c755d1d6
Aman Kumar Jha <amankumar9631400@gmail.com>
2024-12-26 11:10:43 +0530

    Two Sum - Pair with Given Sum

---
+++ GFG POTD/Two Sum - Pair with Given Sum.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target) {
+        
+        unordered_map<int, int> mp;
+        for(auto i=0; i<arr.size(); i++){
+            mp[arr[i]] = i;
+        }
+        
+        for(auto i=0; i<arr.size(); i++){
+            int _a = target - arr[i];
+            if(mp.count(_a) && mp[_a] != i) return true;
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int x;
+        cin >> x;
+        cin.ignore();
+
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        auto ans = ob.twoSum(arr, x);
+        cout << (ans ? "true" : "false") << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
