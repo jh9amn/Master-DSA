5e01d7f33c7d20dffa9ffb53c50b95d127303a00
Aman Kumar Jha <amankumar9631400@gmail.com>
2025-03-17 20:29:44 +0530

    416. Partition Equal Subset Sum

--- DP/416. Partition Equal Subset Sum.cpp
+++ DP/416. Partition Equal Subset Sum.cpp
@@ -27,3 +27,30 @@ public:
         return solve(nums, mid, 0, dp);
     }
 };
+
+
+/*
+Python3
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+
+        if total % 2 != 0:
+            return False
+        
+        target = total // 2
+        
+        @lru_cache(None)            
+        def solve(i, target):
+            if target == 0:
+                return True
+            
+            if i == len(nums) or target < 0:
+                return False
+
+            return solve(i+1, target - nums[i]) or solve(i+1, target)
+        
+        return solve(0, target)
+
+*/
