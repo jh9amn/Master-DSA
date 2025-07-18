0de1935c19aaca2f7a2f80ddec1ee0e93041a2f6
Aman Kumar Jha <amankumar9631400@gmail.com>
2025-07-18 15:36:41 +0530

    Lambda functions in cpp

--- LAMBDA EXPRESSION/LIS.cpp
+++ LAMBDA EXPRESSION/LIS.cpp
@@ -28,13 +28,15 @@ public:
     }
 };
 
+/************************************************************************************/
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>> dp(n, vector<int> (n+1, -1));
 
-        auto solve = [&] (int i, int prev_idx, auto &&self) -> int {
+        auto solve = [&] (int i, int prev_idx, auto &&self) {
             if(i == n) return 0;
             if(dp[i][prev_idx+1] != -1) return dp[i][prev_idx+1];
 
@@ -49,5 +51,48 @@ public:
         };
 
         return solve(0, -1, solve);
+    }
+};
+
+/************************************************************************************/
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+
+        for(int i=n-1; i>=0; i--) {
+            for(int prev=i-1; prev >= -1; prev--) {
+                int take = 0;
+                if(prev == -1 || nums[i] > nums[prev]) {
+                   take = 1 + dp[i+1][i+1];     // prev = i + 1
+                }
+                int skip = dp[i+1][prev+1];
+                dp[i][prev + 1] = max(take, skip);
+            }
+        }
+        return dp[0][0];    // Starting with i=0, prev=-1 → mapped to prev+1 = 0
+    }
+};
+
+/************************************************************************************/
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n+1, 1);
+        int maxi = 1;
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<i; j++) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+                maxi = max(dp[i], maxi);
+            }
+        }
+        return maxi;    // Starting with i=0, prev=-1 → mapped to prev+1 = 0
     }
 };
