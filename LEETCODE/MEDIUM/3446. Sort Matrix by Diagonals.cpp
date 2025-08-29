4138c9607179cfdadc9b83132b9c0b6bd36f1981
Aman Kumar Jha <112791419+jh9amn@users.noreply.github.com>
2025-08-29 20:50:47 +0530

    3446. Sort Matrix by Diagonals

--- LEETCODE/MEDIUM/3446. Sort Matrix by Diagonals.cpp
+++ LEETCODE/MEDIUM/3446. Sort Matrix by Diagonals.cpp
@@ -45,3 +45,53 @@ public:
 
     }
 };
+
+/*
+[
+    [ 0,0  0,1  0,2  0,3 ],
+    [ 1,0  1,1  1,2  1,3 ],
+    [ 2,0  2,1  2,2  2,3 ],
+    [ 3,0  3,1  3,2  3,3 ],
+]
+
+Their difference (i-j) have unique value, store them into map at their diagonal difference
+*/
+
+
+// using map 
+class Solution {
+public:
+    vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        unordered_map<int, vector<int>> mp;
+
+        // traverse on every ele and store them to their diagonal diffrence
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                int diagonalDiff = i-j;
+                mp[diagonalDiff].push_back(grid[i][j]);
+            }
+        }
+
+        for(auto &it: mp) {
+            // do opposite sorting to access ele in O(1)
+            if(it.first >= 0) {
+                sort(begin(it.second), end(it.second));
+            }
+            else{
+                sort(begin(it.second), end(it.second), greater<int> ());
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                // access ele at O(1)
+                int diagonalDiff = i-j;
+                grid[i][j] = mp[diagonalDiff].back();
+                mp[diagonalDiff].pop_back();
+            }
+        }
+
+        return grid;
+    }
+};
