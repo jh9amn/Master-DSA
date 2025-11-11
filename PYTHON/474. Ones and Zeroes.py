6d6822b4c26722f1ae8cf0f05a827e550ea14341
Aman Kumar Jha <112791419+jh9amn@users.noreply.github.com>
2025-11-11 12:01:29 +0530

    Implement memoization and tabulation in solution
    Added memoization and tabulation methods for finding the maximum number of strings that can be formed with given 0s and 1s.

--- PYTHON/474. Ones and Zeroes.py
+++ PYTHON/474. Ones and Zeroes.py
@@ -1,5 +1,7 @@
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        ################## Memoization ############################
+        
         dp = {}
 
         def dfs(i, m, n):
@@ -21,3 +23,29 @@ class Solution:
             return dp[(i, m, n)]
 
         return dfs(0, m, n)
+
+
+
+
+        ################# Tabulation ###############################
+
+
+        dp = defaultdict(int)
+        for i in range(len(strs)):
+            mCnt = strs[i].count("0")
+            nCnt = strs[i].count("1")
+
+            for _m in range(m+1):
+                for _n in range(n+1):
+                    maxi = 0
+                    if mCnt <= _m and nCnt <= _n:
+                        maxi = max(dp[(i-1, _m, _n)],
+                                        1 + dp[(i-1, _m - mCnt, _n - nCnt)])
+                    else:
+                        maxi = dp[(i-1, _m, _n)]
+
+                    dp[(i, _m, _n)] = maxi
+            
+        return dp[(len(strs) - 1, m, n)]
+
+
