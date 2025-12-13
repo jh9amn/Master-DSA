bec58584cc91fdcc376b70a641cdb3d816ba23b8
Aman Kumar Jha <112791419+jh9amn@users.noreply.github.com>
2025-12-13 16:44:20 +0530

    Add path finding solution for directed graph
    Implement BFS to check path existence in a directed graph.

---
+++ PYTHON/Path in directed Graph.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+class Solution:
+    # @param A : integer
+    # @param B : list of list of integers
+    # @return an integer
+    def solve(self, A, B):
+        graph = [[] for _ in range(A + 1)]
+        
+        for u, v in B:
+            graph[u].append(v)
+
+            
+        vis = [False] * (A + 1)
+        vis[1] = True
+        q = deque([1])
+        while q:
+            node = q.popleft()
+            if node == A:
+                return 1
+                
+            for nei in graph[node]:
+                if vis[nei] == False:
+                    vis[nei] = True
+                    q.append(nei)
+                    
+        return 0
+                    
+            
+            
+
+            
