1d3f65322aae702eb0ed841b744448c67558d32e
Aman Kumar Jha <112791419+jh9amn@users.noreply.github.com>
2025-12-13 19:36:44 +0530

    Add DFS solution for pathfinding in directed graph
    Implemented DFS for pathfinding in a directed graph.

--- PYTHON/Path in directed Graph.py
+++ PYTHON/Path in directed Graph.py
@@ -26,7 +26,39 @@ class Solution:
                     
         return 0
                     
+
+
+
+## DFS
+
+import sys
+sys.setrecursionlimit(10**7)
+
+class Solution:
+    # @param A : integer
+    # @param B : list of list of integers
+    # @return an integer
+    def solve(self, A, B):
+        graph = [[] for _ in range(A+1)]
+        
+        for u, v in B:
+            graph[u].append(v)
+        
+        vis = [False] * (A+1)
+        vis[1] = True
+        
+        def dfs(node):
+            if node == A:
+                return True
+            for nei in graph[node]:
+                if vis[nei] == False:
+                    vis[nei] = True
+                    if dfs(nei):
+                        return True
+            return False
             
+        return 1 if dfs(1) else 0
+        
             
 
             
