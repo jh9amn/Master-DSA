a24326cb3eba1ce681fd55370d74f01b86eb7f33
Aman Kumar Jha <112791419+jh9amn@users.noreply.github.com>
2026-05-23 12:20:45 +0530

    Add solution for Longest Happy Prefix using KMP
    Implemented KMP algorithm to find the longest happy prefix.

---
+++ ALGORITHM/1392. Longest Happy Prefix.cpp
@@ -0,0 +1,34 @@
+/*
+Here, we can use KMP algorithm LPS (Longest Prefix Suffix)
+*/
+
+class Solution {
+public:
+    string longestPrefix(string s) {
+        int n = s.size();
+        vector<int> lps(n, 0);
+        int i=1;
+        int len = 0;
+
+        // Build LPS Array
+        // TC O(N)
+        // SC O(N)
+        while(i < n) {
+            if(s[i] == s[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else {
+                if(len > 0) {
+                    len = lps[len-1];
+                }
+                else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return s.substr(0, lps[n-1]);
+    }
+};
