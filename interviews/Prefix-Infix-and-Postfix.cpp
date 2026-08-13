09bedb3df24926f12ec424f9aabda89e9cf6845d
Aman Kumar Jha <112791419+jh9amn@users.noreply.github.com>
2026-08-13 11:58:17 +0530

    Refactor infixToPostfix for better expression handling
    Fix infixToPostfix function to handle parentheses and operators correctly.

--- interviews/Prefix-Infix-and-Postfix.cpp
+++ interviews/Prefix-Infix-and-Postfix.cpp
@@ -4,8 +4,32 @@ using namespace std;
 class Expression {
 
   string infixToPostfix(string &s){
+          
+    if( (s[i] >= 'a' and s[i] <= 'z') || (s[i] >= 'A' and s[i] <= 'Z') || (s[i] >= '1' and s[i] <= '9')) {
+      ans[i] += s[i];
+    }
+    else if( s[i] == '(') {
+      st.push('(');
+    }
 
-    if
+    else if(s[i] == ')'){
+      while(st.top() != '(') {
+        ans += st.top();
+      }
+      st.pop();
+    }
+
+    else {
+      while(!st.empty() && precedence(s[i]) <= precedence(st.top())){
+        ans += st.top();
+        st.pop();
+      }
+      st.push(s[i]);
+    }
   }
-
+  while(!st.empty()) {
+    ans += st.top();
+    st.pop();
+  }
+  return ans;
 }
