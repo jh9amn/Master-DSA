b03a50ac462d239302e6921fd9c4c65c1e19a247
Aman Kumar Jha <112791419+jh9amn@users.noreply.github.com>
2026-08-16 00:49:24 +0530

    Refactor expression conversion methods
    Refactor infix to postfix and prefix conversion methods. Implement operator precedence and operand checking.

--- interviews/Prefix-Infix-and-Postfix.cpp
+++ interviews/Prefix-Infix-and-Postfix.cpp
@@ -3,33 +3,167 @@ using namespace std;
 
 class Expression {
 
-  string infixToPostfix(string &s){
-          
-    if( (s[i] >= 'a' and s[i] <= 'z') || (s[i] >= 'A' and s[i] <= 'Z') || (s[i] >= '1' and s[i] <= '9')) {
-      ans[i] += s[i];
-    }
-    else if( s[i] == '(') {
-      st.push('(');
-    }
-
-    else if(s[i] == ')'){
-      while(st.top() != '(') {
-        ans += st.top();
-      }
-      st.pop();
+    int precedence(char c){
+        if(c == '^')
+            return 3;
+        else if(c == '*' || c == '/')
+            return 2;
+        else if(c == '+' || c == '-')
+            return 1;
+        else
+            return -1;
     }
 
-    else {
-      while(!st.empty() && precedence(s[i]) <= precedence(st.top())){
-        ans += st.top();
-        st.pop();
-      }
-      st.push(s[i]);
+    bool isOperand(char c) {
+        return (isalpha(c) or isdigit(c));
     }
-  }
-  while(!st.empty()) {
-    ans += st.top();
-    st.pop();
-  }
-  return ans;
-}
+
+    // INFIX to POSTFIX
+    string infixToPostfix(string &s){
+        stack<char> st;
+        string ans = "";
+        for (int i = 0; i < s.size(); i++){
+
+            // operand
+            if (isOperand(s[i])) {
+                ans += s[i];
+            }
+
+            // opening bracket
+            else if (s[i] == '(')
+            {
+                st.push('(');
+            }
+        
+            // closing bracket
+            else if (s[i] == ')')
+            {
+                while (!st.empty() and st.top() != '(')
+                {
+                    ans += st.top();
+                    st.pop();
+                }
+                if(!st.empty())
+                    st.pop();
+            }
+            
+            // operator
+            else
+            {
+                if(s[i] == '^') {
+                    //   ^ is right associative
+                    while(!st.empty() and precedence(s[i]) < precedence(st.top())){
+                        ans += st.top();
+                        st.pop();
+                    }
+                }
+                else {
+                    //    + - * / are left associative
+                    while(!st.empty() and precedence(s[i]) <= precedence(st.top())) {
+                        ans += st.top();
+                        st.pop();
+                    }
+                }
+                st.push(s[i]);
+            }
+        }
+        
+        // remaining operators in the stack
+        while(!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+        return ans;
+    }
+
+
+
+/*
+    **INFIX to PREFIX**
+
+    Ex: (A+B)*C-D+F
+
+    Step 1: reverse the infix
+        F+D-C*)B+A(
+        F+D-C*(B+A)         Note: put '(' at place of ')' and viseversa
+    
+    Step 2: Infix to postfix
+        
+    Step 3: Reverse the answer 
+
+
+    Infix
+    ↓
+    Reverse
+    ↓
+    Swap '(' and ')'
+    ↓
+    Convert to postfix
+    ↓
+    Reverse postfix
+    ↓
+    Prefix
+
+*/
+
+    string infixToPrefix(string &s){
+        reverse(s.begin(), s.end());
+        for (int i = 0; i < s.size(); i++){
+            if (s[i]== '('){
+                s[i] = ')';
+            }
+            else if (s[i] == ')') {
+                s[i] = '(';
+            }  
+        }   
+
+        // Remaining code same as infixToPostfix function
+        // string postfix = infixToPostfix(s);
+        // reverse(postfix.begin(), postfix.end());
+        // return postfix;
+
+        stack<char> st;
+        string ans = "";
+
+        for (int i = 0; i < s.size(); i++){
+            if(isOperand(s[i])){
+                ans += s[i];
+            }
+
+            else if(s[i] == '(') {
+                st.push('(');
+            }
+            else if(s[i] == ')'){
+                while(!st.empty() && st.top() != '(') {
+                    ans += st.top();
+                    st.pop();
+                }
+                if(!st.empty()) {   // pop the '(' from the stack
+                    st.pop();
+                }
+            }
+            else {
+                if (s[i] == '^') {
+                    while(!st.empty() && precedence(s[i]) <= precedence(st.top())) {
+                        ans += st.top();
+                        st.pop();
+                    }
+                }
+                else {
+                    while(!st.empty() && precedence(s[i]) < precedence(st.top())) {
+                        ans += st.top();
+                        st.pop();
+                    }
+                }
+                st.push(s[i]);
+            }
+        }
+        while(!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+        reverse(begin(ans), end(ans));
+        return ans;
+    }
+
+};
