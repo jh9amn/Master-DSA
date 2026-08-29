2aa6654a9794f8eaa97355e210637a824ffdf03d
Aman Kumar Jha <112791419+jh9amn@users.noreply.github.com>
2026-08-29 19:05:03 +0530

    Add topological sort function in Topological Sort.cpp
    Implemented topological sort using Kahn's algorithm.

---
+++ striver-sde-sheet/graph/Topological Sort.cpp
@@ -0,0 +1,45 @@
+class Solution {
+  public:
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // code here
+        /*
+        ** linear ordering of vertices such that if there is an edge between u & v, u always appears before v in the ordering.
+        **Topological sort can onluy possible in DAG
+        */
+        
+        unordered_map<int, vector<int>> adj;
+        
+        for(auto it: edges){
+            adj[it[0]].push_back(it[1]);
+        }
+        
+        int indegree[V] = {0};
+        for(auto it: edges) {
+            indegree[it[1]]++;
+        }
+        
+        queue<int>q;
+        vector<int> toposort;
+        
+        for(int i=0; i<V; i++) {
+            if(indegree[i] == 0) q.push(i); // start the toposort from here
+        }
+        
+        // TC = O(V+E)
+        while(!q.empty()){
+           int node = q.front();
+           q.pop();
+          
+           toposort.push_back(node);
+          
+            for(auto it: adj[node]) {
+                indegree[it]--;
+                if(indegree[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+        
+        return toposort;
+    }
+};
