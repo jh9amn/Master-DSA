a8663e6c4e2a2fe03e00d5fc944f79b7a8093ad4
Aman Kumar Jha <112791419+jh9amn@users.noreply.github.com>
2026-08-29 11:56:28 +0530

    Add initial C++ template with common includes

---
+++ compi-templete.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using vvi = vector<vector<int>>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vb = vector<bool>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using tiii = tuple<int, int, int>;
+
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define yes cout << "YES" << '\n';
+#define no cout << "NO" << '\n';
+#define pb push_back
+
+void fastio(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main(){}
